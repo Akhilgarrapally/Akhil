e4f496b766ee2a4291a4ad2568a63798c793be72
G.Akhil <71013647+Akhilgarrapally@users.noreply.github.com>
2021-11-22 15:22:48 +0530

    Delete ddddd.py

--- ddddd.py
+++
@@ -1,9 +0,0 @@
-import socketserver
-import http.server
-import ssl
-
-httpd = socketserver.TCPServer(('0.0.0.0', 10000), http.server.SimpleHTTPRequestHandler)
-
-httpd.socket = ssl.wrap_socket(httpd.socket, certfile=("/root/Desktop/server.pem"), keyfile="/root/Desktop/server.key", server_side=True)
-
-httpd.serve_forever()
