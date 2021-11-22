244fd480a6a98b9d357b1dbe79a2997eec20abf5
G.Akhil <71013647+Akhilgarrapally@users.noreply.github.com>
2021-11-22 15:20:57 +0530

    Delete route.py

--- route.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask  
-app = Flask(__name__)  
- 
-@app.route('/home')  
-def home():  
-    return 'hello welcome';  
-  
-if __name__ =="__main__":  
-    app.run(debug=(True))  
